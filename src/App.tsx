9caca9c5b47a0d2beba9b8b55746c193e2a28dd9
henrique <hickm018@gmail.com>
2022-05-13 02:09:02 -0300

    refactor: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import CreateOrUpdateUser from './components/UserCreateorUpdate';
+import Table from './components/Table';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <Table /> } />
+        <Route path='/users/create' element={ <CreateOrUpdateUser /> } />
+        <Route path='/users/update/:id' element={ <CreateOrUpdateUser /> } />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
